56617d72945cfec7646c871b05cea2389a253c58
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-05-08 09:19:30 +0600

    list

---
+++ list.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <list>
+using namespace std;
+int main() {
+
+    // Create a list
+    list<string> cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+    // Access list
+    cars.front();
+    cars.back();
+
+    // Change list element
+    cars.front() = "Toyota";
+    cars.back() = "Tesla";
+
+    // Add list element
+    cars.push_back("Lamborgini");
+
+    // Remove list element
+    cars.pop_back();
+
+    // List size
+    cars.size();
+
+    // Check if list is empty
+    cars.empty();
+
+    // Loop through a list
+    for(string car : cars) {
+        cout << car << endl;
+    }
+
+    return 0;
+}
