f305b58abb5eaf74c72814e81d7788148ccec7a6
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-05-22 09:46:18 +0600

    class

---
+++ class.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class student {
+    public:
+    string name;
+    int roll;
+};
+
+int main() {
+
+    student s1, s2, s3;
+    s1.name = "Samiul";
+    s1.roll = 1;
+
+    s2.name = "Nawshin";
+    s2.roll = 2;
+
+    s3.name = "Simanto";
+    s3.roll = 3;
+
+    cout << s3.name << " " << s3.roll << endl;
+
+    return 0;
+}
