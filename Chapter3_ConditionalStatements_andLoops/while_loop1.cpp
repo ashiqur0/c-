1ebe44e1895c8869f951eeb0303f3c2ff7a735b5
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-04 22:08:16 +0600

    1 to 5 using while loop

---
+++ Chapter3_ConditionalStatements_andLoops/while_loop1.cpp
@@ -0,0 +1,13 @@
+// print numbers 1 to 5
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int count = 1;
+    while(count <= 5) {
+        cout << count++ << " ";
+    }
+
+
+  return 0;
+}
