a6ffd87ddc6637da14c39aa151baf85de84e6419
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-05 01:22:13 +0600

    Sum till N numbers which are divisible by 3 using for loop

---
+++ Chapter3_ConditionalStatements_andLoops/sum_till_N_divisible_by_3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int n = 10, sum = 0;
+    for(int i=3; i<=10; i+=3) {
+        sum += i;
+    }
+
+    cout << sum << endl;
+
+  return 0;
+}
