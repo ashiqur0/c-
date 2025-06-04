f7ee849f0fb5b8690e48af9afe7f3fd80937ba25
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-04 21:27:36 +0600

    Eligible to vote or not

---
+++ Chapter3_ConditionalStatements_andLoops/vote.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int age;
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "Elegible to vote\n";
+    } else {
+        cout << "Not elegible to vote\n";
+    }
+
+    return 0;
+}
