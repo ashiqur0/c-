7592f69373192fed78480dce658646d62a2ed8e5
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-04 22:20:25 +0600

    Sum of first 5 numbers

---
+++ Chapter3_ConditionalStatements_andLoops/for2.cpp
@@ -0,0 +1,13 @@
+// sum of first 5 numbers
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int sum = 0;
+    for(int i=1; i<=5; i++){
+        sum += i;
+    }
+
+    cout << sum << endl;
+
+  return 0;
+}
