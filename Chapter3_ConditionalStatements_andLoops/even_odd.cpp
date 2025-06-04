6edc417190923ee2af1e5d4073fbd7c090dc7e7c
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-04 21:53:45 +0600

    Even Odd with Ternary

---
+++ Chapter3_ConditionalStatements_andLoops/even_odd.cpp
@@ -0,0 +1,11 @@
+// Check the given number is even or odd
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int nums;
+    cin >> nums;
+    cout << (nums % 2 == 0? "Even\n" : "Odd\n");
+
+  return 0;
+}
