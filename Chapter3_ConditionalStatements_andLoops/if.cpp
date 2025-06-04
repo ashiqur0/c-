a7ae4bd3178ed8e84818b25488da057dd19fc4f2
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-04 21:23:11 +0600

    positive or negative

---
+++ Chapter3_ConditionalStatements_andLoops/if.cpp
@@ -0,0 +1,15 @@
+// Check the give number is positive of negative
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int n;
+    cin >> n;
+    if(n >= 0) {
+        cout << n << " is a positive number\n";
+    } else {
+        cout << n << " is a negative number\n";
+    }
+
+  return 0;
+}`
