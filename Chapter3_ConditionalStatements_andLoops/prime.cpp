2001d8fb6e258b9e750ef52b9b2cc31f32733f56
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-04 23:40:22 +0600

    Prime or Non-prime

--- Chapter3_ConditionalStatements_andLoops/prime.cpp
+++ Chapter3_ConditionalStatements_andLoops/prime.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    // int n=4;
-    int n=5;
+    int n=3;
+    // int n=5;
     bool isPrime = true;
-    for(int i=2; i<n; i++) {
+    for(int i=2; i*i<=n; i++) {
         if(n % i == 0) {
             isPrime = false;
             break;
