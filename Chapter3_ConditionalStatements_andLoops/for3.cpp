c5e365156b5c8ef9f35ea1e3819c59914f81fa54
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-04 23:05:00 +0600

    sum of even till n using for loop

---
+++ Chapter3_ConditionalStatements_andLoops/for3.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int sum = 0, n=10;
+    for(int i=0; i<=n; i+=2) {
+        sum += i;
+    }
+    cout << sum << endl;
+
+  return 0;
+}
