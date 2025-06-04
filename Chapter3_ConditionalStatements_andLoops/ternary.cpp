6b62d9d2fe6e2630bb5d9ca401808135ec9fcfad
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-04 21:51:14 +0600

    Positve or Negative with Ternary Statement

---
+++ Chapter3_ConditionalStatements_andLoops/ternary.cpp
@@ -0,0 +1,12 @@
+// Ternary Statement: Check the given number is positive or negative 
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int nums;
+    cin >> nums;
+    cout << (nums >= 0? "Positive\n" : "Negative\n");
+
+  return 0;
+}
