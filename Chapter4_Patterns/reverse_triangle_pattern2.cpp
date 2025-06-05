b9f9e5c516c44634352d13ff1dfcdc17df09c762
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-05 14:59:17 +0600

    Reverse Triangle Pattern with Characte

---
+++ Chapter4_Patterns/reverse_triangle_pattern2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+// A
+// B A
+// C B A
+// D C B A
+int main() {
+
+    int n = 4;
+    for(int i=1; i<=n; i++) {
+        char ch = 65+i-1;
+        for(int j=i; j>=1; j--) {
+            cout << ch-- << " ";
+        }
+        cout << endl;
+    }
+
+  return 0;
+}
