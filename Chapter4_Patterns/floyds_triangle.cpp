cef18a25938f4f6a583f5f1966807e9dcb1c2e44
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-05 14:45:17 +0600

    Floyd

---
+++ Chapter4_Patterns/floyds_triangle.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+int main() {
+
+    int n=4, count = 1;
+    for(int i=1; i<=n; i++) {
+        for(int j=i; j>=1; j--) {
+            cout << count++ << " ";
+        }
+        cout << endl;
+    }
+
+  return 0;
+}
