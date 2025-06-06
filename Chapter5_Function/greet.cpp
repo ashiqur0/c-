3bf7764764404733f517e5ca3c71ea45173f9dad
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-06 14:59:51 +0600

    Gree a person

---
+++ Chapter5_Function/greet.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void greet(string name) {
+    cout << "Hello, " << name << endl;
+}
+
+int main() {
+
+  string name = "Masud Rana";
+  greet(name);
+
+  return 0;
+}
