493b3b89f8985a4220e2105e3ea8e82f1e0477e4
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-06 13:16:11 +0600

    int type

---
+++ Chapter5_Function/return_type2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sum(int num1, int num2) { //int: return an integer value to the calling function.
+    return num1 + num2;
+}
+int subtract(int num1, int num2) { 
+    return num1 - num2;
+}
+int multiplication(int num1, int num2) { 
+    return num1 * num2;
+}
+int division(int num1, int num2) { 
+    return num1 / num2;
+}
+int modulo(int num1, int num2) { 
+    return num1 % num2;
+}
+
+int main() {
+
+    int num1 = 10, num2 = 5;
+    cout << sum(num1, num2) << endl;
+    cout << subtract(num1, num2) << endl;
+    cout << multiplication(num1, num2) << endl;
+    cout << division(num1, num2) << endl;
+    cout << modulo(num1, num2) << endl;
+
+  return 0;
+}
