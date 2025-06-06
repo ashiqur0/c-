3f9e486d33996b89c65a249b3b951a179b41a8a9
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-06 12:58:41 +0600

    Void Function

---
+++ Chapter5_Function/hello.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printhello() {     // Declaration + Definition
+    cout << "Hello" << endl;
+}
+int main() {
+
+    printhello();       // Calling or Invocking
+
+  return 0;
+}
