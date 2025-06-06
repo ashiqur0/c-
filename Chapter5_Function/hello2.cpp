61df0439cb2d31047429bdccee061064401dc3e7
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-06 13:01:34 +0600

    Function Declaration Definition Calling

---
+++ Chapter5_Function/hello2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printHello();      // Declaration
+
+int main() {
+
+    printHello();       // Calling 
+
+  return 0;
+}
+void printHello() {     // Definition
+    cout << "Hello" << endl;
+}
