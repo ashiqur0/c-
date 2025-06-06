e7815d902c50d8ca99c6c5ff32e6a2bf94d51f32
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-06 13:05:51 +0600

    Argument Parameter

---
+++ Chapter5_Function/argument_parameter.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void greet(string name) {       // name is a parameter
+    cout << "Hello, " << name << endl;
+}
+
+int main() {
+
+    string name = "Rana";
+    // cin >> name;
+    greet(name);                // name is a argument
+
+  return 0;
+}
