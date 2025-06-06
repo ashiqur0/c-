9e565b8ba2c138fb313b48e7c4b0e0cd5ec51975
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-06 13:09:26 +0600

    void type

---
+++ Chapter5_Function/return_type.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printHello() { //void: does not return anything to calling function. Just perform the task.
+    cout << "hello" << endl;
+}
+
+
+int main() {
+
+    printHello();
+
+  return 0;
+}
