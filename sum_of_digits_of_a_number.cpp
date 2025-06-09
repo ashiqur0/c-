bfc1f3291f8db708d4eee71d138d77c0b8b0d400
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-09 16:32:54 +0600

    Sum of Digits of a Number

---
+++ sum_of_digits_of_a_number.cpp
@@ -0,0 +1,17 @@
+// Calculate The Sum of Digits of a Number
+#include <bits/stdc++.h>
+using namespace std;
+int sumofdigits(int n) {
+    int sum = 0;
+    while(n) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+int main() {
+
+    cout << sumofdigits(145) << endl;
+
+  return 0;
+}
