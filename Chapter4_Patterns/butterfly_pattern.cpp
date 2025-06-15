46d73e00e51c72cddfff9cb1ed73da256c71cafe
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-15 07:09:20 +0600

    Pattern: Butterfly Pattern

--- Chapter4_Patterns/butterfly_pattern.cpp
+++ Chapter4_Patterns/butterfly_pattern.cpp
@@ -1,41 +1,50 @@
+/*
+Pattern: Butterfly Pattern
+
+*             *
+* *         * *
+* * *     * * *
+* * * * * * * *
+* * * * * * * *
+* * *     * * *
+* *         * *
+*             *
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
-// *             *
-// * *         * *
-// * * *     * * *
-// * * * * * * * *
-// * * * * * * * *
-// * * *     * * *
-// * *         * *
-// *             *
+
 int main() {
-    int n=4;
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<=i; j++) {
+
+    int n = 4;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++) {
             cout << " *";
         }
-        for(int j=n-i-1; j>0; j--) {
+        for(int j = n - i - 1; j > 0; j--) {
             cout << "  ";
         }
-        for(int j=n-i-1; j>0; j--) {
+        for(int j = n - i - 1; j > 0; j--) {
             cout << "  ";
         }
-        for(int j=0; j<=i; j++) {
+        for(int j = 0; j <= i; j++) {
             cout << " *";
         }
         cout << endl;        
     }
-    for(int i=0; i<n; i++) {
-        for(int j=n-i; j>0; j--) {
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = n - i; j > 0; j--) {
             cout << " *";
         }
-        for(int j=0; j<i; j++) {
+        for(int j = 0; j < i; j++) {
             cout << "  ";
         }
-        for(int j=0; j<i; j++) {
+        for(int j = 0; j < i; j++) {
             cout << "  ";
         }
-        for(int j=n-i; j>0; j--) {
+        for(int j = n - i; j > 0; j--) {
             cout << " *";
         }
         cout << endl;
