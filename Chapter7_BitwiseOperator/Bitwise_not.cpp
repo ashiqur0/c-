82a6ba3cd8c65bb08cb4f6cd20719167a76d560e
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-15 06:53:58 +0600

    Bitwise Operator: Bitwise NOT (~)

---
+++ Chapter7_BitwiseOperator/Bitwise_not.cpp
@@ -0,0 +1,19 @@
+/*
+Bitwise Operator: Bitwise NOT (~)
+~ 0 = -1
+~ 1 = 2
+~ 15 = -16
+
+Example 1: ~ 4
+~ 4 = -5 in Decimal
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int a = 4;
+    cout << (~ a) << endl;
+
+  return 0;
+}
