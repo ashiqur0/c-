c969c7a46a6eb1aca477813dd6967852d137d4a4
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-15 06:42:04 +0600

    Bitwise Operator: Bitwise XOR

---
+++ Chapter7_BitwiseOperator/Bitwise_xor.cpp
@@ -0,0 +1,23 @@
+/*
+Bitwise Operator: Bitwise XOR
+0 0 = 0
+0 1 = 1
+1 0 = 1
+1 1 = 0
+
+Example 1: 4 ^ 8
+4 = 0100
+8 = 1000
+&_______
+    1100 = 12 in Decimal
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int a = 4, b = 8;
+    cout << (a ^ b) << endl;
+
+  return 0;
+}
