e55469c68e548ae540bb149517384676e515332a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-15 10:35:05 +0600

    Write a function to reverse an integer

--- Chapter7_BitwiseOperator/hw2_reverseInt.cpp
+++ Chapter7_BitwiseOperator/hw2_reverseInt.cpp
@@ -5,7 +5,7 @@ HW2: Write a function to reverse an integer
 #include <bits/stdc++.h>
 using namespace std;
 
-int reverse(int n) {
+int reverse_method_1(int n) {
     int rev = 0;
     while(n) {
         rev = rev * 10 + n % 10;
@@ -14,10 +14,23 @@ int reverse(int n) {
     return rev;
 }
 
+int reverse_method_2(int n) {
+    int ans = 0;
+    while(n) {
+        int digit = n % 10;
+        n /= 10;
+        if (ans > INT_MAX / 10 || (ans == INT_MAX && digit > 7)) return 0;
+        if (ans < INT_MIN / 10 || (ans == INT_MIN && digit < -8)) return 0;
+        ans = ans * 10 + digit;
+    }
+    return ans;
+}
+
 int main() {
 
-    int n = 123;
-    cout << reverse(n) << endl;
+    // int n = 123;    // 321
+    int n = 1534236469;    // 0
+    cout << reverse_method_2(n) << endl;
 
   return 0;
 }
