18504ae767378fd79c9657576dbe2b0968dba2b4
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-15 06:37:58 +0600

    Bitwise Operator: Bitwise AND

--- Chapter7_BitwiseOperator/Bitwise_and.cpp
+++ Chapter7_BitwiseOperator/Bitwise_and.cpp
@@ -1,3 +1,16 @@
+/*
+Bitwise Operator: Bitwise AND
+0 0 = 0
+0 1 = 0
+1 0 = 0
+1 1 = 1
+
+4 = 0100
+8 = 1000
+&_______
+    0000 = 0 in Decimal
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
