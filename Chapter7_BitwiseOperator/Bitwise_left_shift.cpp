9802ca85219aabd61e5875ef1872f0675bb461e7
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-15 07:24:15 +0600

    Bitwise Operator: Bitwise Left Shif (<<)

---
+++ Chapter7_BitwiseOperator/Bitwise_left_shift.cpp
@@ -0,0 +1,22 @@
+/*
+Bitwise Operator: Bitwise Left Shif (<<)
+0 << 1 = 00
+1 << 1 = 10
+1 << 2 = 100
+101 << 2 = 10100
+
+Example 1: 2 << 3
+1 = 10 in Binary
+1 << 3 = 1 << 3 = 1000 = 8 in decimal
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int a = 1;
+    cout << (a << 3) << endl;
+    cout << (5 << 2) << endl; // 20
+
+  return 0;
+}
