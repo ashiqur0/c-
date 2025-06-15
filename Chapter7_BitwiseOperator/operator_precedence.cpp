5f3b78770255d8e450e74fcfd201bb473bc95a84
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-15 08:40:52 +0600

    Bitwise Operator: Operator Precedence and Associativity

---
+++ Chapter7_BitwiseOperator/operator_precedence.cpp
@@ -0,0 +1,23 @@
+/*
+Bitwise Operator: Operator Precedence and Associativity
+
+Associativity           Precedence
+Right to Left           !, ~, ++, --
+Left ot Right           *, /, %
+Left ot Right           +, -
+Left ot Right           <, <=, >=, >
+Left ot Right           ==, !=
+Left ot Right           &&
+Left ot Right           ||
+Right to Left           =
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    cout << 5 - 2 * 3 << endl;  // -1   
+    cout << (5 - 2) * 3 << endl;  // 9
+
+  return 0;
+}
