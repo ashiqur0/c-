f8dd99bdfd6401e24bf9cfabaa9449b636ebbd4e
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-16 15:30:04 +0600

    Array : Find smallest

--- Chapter8_Array/find_smallest.cpp
+++ Chapter8_Array/find_smallest.cpp
@@ -9,9 +9,10 @@ int main() {
     int size = 6;
     int smallest = INT_MAX;
     for(int i = 0; i < size; i++) {
-        if(nums[i] < smallest) {
-            smallest = nums[i];
-        }
+        // if(nums[i] < smallest) {
+        //     smallest = nums[i];
+        // }
+        smallest = min(smallest, nums[i]);
     }
 
     cout << smallest << endl;
