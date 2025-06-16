0ff95f14dd750e6f6d8c7e93f62c56983196a583
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-16 16:29:48 +0600

    Array : Reverse array [Two Pointer Approach]

--- Chapter8_Array/reverse_array.cpp
+++ Chapter8_Array/reverse_array.cpp
@@ -5,9 +5,11 @@ using namespace std;
 
 void rerverseArray(int arr[], int size) {
     for (int i = 0; i < size / 2; i++) {
-        int temp = arr[i];
-        arr[i] = arr[size - i - 1];
-        arr[size - i - 1] = temp;
+        // int temp = arr[i];
+        // arr[i] = arr[size - i - 1];
+        // arr[size - i - 1] = temp;
+        swap(arr[i], arr[size - i - 1]);
+        // Two Pointer Approach: start = i, end = size - i - 1
     }
 }
 
