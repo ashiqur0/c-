dd7cd3995a047fc4ffda07f12684982f12cac060
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-16 16:07:16 +0600

    Array : Linear search algorithm

---
+++ Chapter8_Array/linear_searh.cpp
@@ -0,0 +1,24 @@
+// Array : Linear search algorithm
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int linearSsearch(int arr[], int target, int size) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+
+    int arr[] = {4, 2, 7, 8, 1, 2, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 5;
+    
+    cout << linearSsearch(arr, target, size) << endl;
+
+  return 0;
+}
