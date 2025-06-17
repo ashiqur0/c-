6865f6d13fbaa182308d84bac64056812d52c3aa
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-17 13:07:21 +0600

    Array : HW-1 Linear Search on vector

---
+++ C++ STL/Lecture9_Vectors/linear_search.cpp
@@ -0,0 +1,23 @@
+// Array : HW-1 Linear Search on vector
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int linear_search(vector<int> vec, int target) {
+    for(int i = 0; i < vec.size(); i++) {
+        if (vec[i] == target) return i;
+    }
+    return -1;
+}
+
+int main() {
+
+    vector<int> vec = {2, 3, 4, 5, 6};
+    int target = 4;
+    if (linear_search(vec, target) != 1) {
+        cout << target << " is found." << endl;
+    }
+
+  return 0;
+}
